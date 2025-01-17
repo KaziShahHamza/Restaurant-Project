98002f0171ef556140ac05d8edf6ab4a017c23f6
soulfulDreamer <kazishahhamza@outlook.com>
2025-01-18 02:06:19 +0600

    update: Hero Section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -21,7 +21,7 @@ const HeroSection = () => {
       <div className="absolute inset-0 -z-10 bg-gradient-to-b from-transparent from-70% to-black"></div>
       <div className="relative z-20 flex h-screen flex-col justify-end pb-20">
         <img src={logo} alt="restaura" className="w-full p-4" />
-        <p className="p-4 text-lg tracking-tighter text-white">Paris</p>
+        <p className="p-4 text-lg tracking-tighter text-white">Dhaka</p>
       </div>
     </section>
   );
