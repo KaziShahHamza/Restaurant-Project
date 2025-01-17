77d5225fc50a4f84eaa39f19633e817030377ae0
soulfulDreamer <kazishahhamza@outlook.com>
2025-01-18 01:13:37 +0600

    feat: Add Dishes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "./components/HeroSection";
 import NavBar from "./components/NavBar";
+import Dishes from "./components/Dishes";
 
 const App = () => {
   return (
     <main className="overflow-y-hidden text-neutral-200 antialiased">
       <HeroSection /> 
-      <NavBar />
+      <NavBar /> 
+      <Dishes />
     </main>
   );
 };
