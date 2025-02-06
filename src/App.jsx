4d80b9c703d92426ce1702738fb73a701b1ee1a2
soulfulDreamer <kazishahhamza@outlook.com>
2025-02-06 20:45:15 +0600

    Add about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import HeroSection from "./components/HeroSection";
 import NavBar from "./components/NavBar";
 import Dishes from "./components/Dishes";
+import About from "./components/About";
 
 const App = () => {
   return (
     <main className="overflow-y-hidden text-neutral-200 antialiased">
       <HeroSection /> 
-      <NavBar /> 
+      <NavBar />
       <Dishes />
+      <About />
     </main>
   );
 };
